ff4307afd757cdc9b214cf2700f875d6f7728006
Maël Le Gall <mael.le_gall@tutanota.com>
2021-09-15 15:13:13 +0100

    Simplify and harmonize xml processing + add handling in sections and paragraph

--- DF_NLP/api.py
+++ DF_NLP/api.py
@@ -1,7 +1,7 @@
 """Module handling process of API request answers."""
 
-import xml.etree.ElementTree as ET
 import re
+import xml.etree.ElementTree as ET
 from typing import Literal
 
 # Determined API type
@@ -19,81 +19,137 @@ def _process_xml(answer: str) -> str:
     Returns:
         Harmonized XML string as answer.
     """
+    # Flatten the XML
+    answer = re.sub(r'\n\s*', " ", answer)
     # Clean non-body tags
-    answer = re.sub("<body>.+(\n.+)*<body>", "<body>", answer)
-    answer = re.sub("</body>.+(\n.+)*</body>", "</body>", answer)
+    answer = re.sub("<body>.+?<body>", "<body>", answer)
+    answer = re.sub("</body>.+?</body>", "</body>", answer)
     # Remove tags cutting text content
-    answer = re.sub(r',?<ce:cross-ref[ \w="]+>', "", answer)
-    answer = re.sub("</ce:cross-ref>", "", answer)
-    answer = re.sub(r'((, )|–)?</?xref[- \w="]*>', "", answer)
-    answer = re.sub(r" (\[\d+\])+", "", answer)
-    answer = re.sub(r'<xref[- \w="]+/>', "", answer)
-    answer = re.sub("<(?=fn )", "\n<", answer)
-    answer = re.sub("(?<=</fn)>", ">\n", answer)
-    answer = re.sub("\n<fn .+\n", "", answer)
-    answer = re.sub(r'<ce:footnote[ \w="]+>(\n.+){3}</ce:footnote>',
+    answer = re.sub(r'</?fn[ \w="]*>', "", answer)
+    answer = re.sub(r'<ce:float-anchor[ \w="]*/>', "", answer)
+    answer = re.sub(r'</?ext-link[- \w=":/.%@#?&;,\[\]()~+*!\'$]*>',
                     "", answer)
-    answer = re.sub(r'(\n\s+)?<ce:float-anchor[ \w="]+/>', "", answer)
-    answer = re.sub(r'<ce:inter-ref[ \w=":/.]+>[\w:/.]+</ce:inter-ref>',
-                    "", answer)
-    answer = re.sub("<ce:display>(\n.+){5}</ce:display>", "", answer)
-    answer = re.sub(r'</?ext-link[- \w=":/.,?&;%#~]*>', "", answer)
-    answer = re.sub("</?bold>", "", answer)
-    answer = re.sub("</?uri>", "", answer)
-    answer = re.sub("</?italic>", "", answer)
-    answer = re.sub("</?sub>", "", answer)
+    answer = re.sub(r'<ce:hsp[ \w=".]*/>', "", answer)
+    answer = re.sub("</?(ce:)?bold>", "", answer)
+    answer = re.sub(r'</?uri[- \w=":/.%@#?&;,\[\]()~+*!\'$]*>', "", answer)
+    answer = re.sub("</?(ce:)?italic>", "", answer)
+    answer = re.sub("</?(ce:)?monospace>", "", answer)
+    answer = re.sub(r' ?<(ce:)?sup[ \w="]*> ?', "^", answer)
+    answer = re.sub(r' ?<(ce:)?(inf|sub)( [ \w="]*)?> ?', "_", answer)
+    answer = re.sub(r'</(ce:)?(sup|inf|sub)>', "", answer)
+    answer = re.sub("</?ce:small-caps>", "", answer)
+    answer = re.sub(r'<ce:textbox[ \w="]*>.+?</ce:textbox>', "", answer)
+    answer = re.sub(r'</?ce:enunciation[ \w="]*>', "", answer)
+    answer = re.sub("</?ce:underline>", "", answer)
+    answer = re.sub("</?ce:sans-serif>", "", answer)
+    answer = re.sub(r'<ce:vsp[ \w=".]*/>', "", answer)
     answer = re.sub("</?sc>", "", answer)
-    answer = re.sub("<sup>", "^", answer)
-    answer = re.sub("</sup>", "", answer)
-    answer = re.sub("</?ack>", "", answer)
+    answer = re.sub("</?ce:cross-out>", "", answer)
+    answer = re.sub('<ce:glyph name="sbnd"/>', "-", answer)
+    answer = re.sub(r'<ce:anchor[ \w="]*/>', "", answer)
+    answer = re.sub(r'</?boxed-text[ \w="]*>', "", answer)
+    answer = re.sub("<(def|term)-head>.+?</(def|term)-head>", "", answer)
     answer = re.sub("\xa0", " ", answer)
-    # Need to add carriage return...
-    answer = re.sub(">(?=<label>)", ">\n", answer)
-    answer = re.sub("(?<=</label>)<", "\n<", answer)
-    answer = re.sub(">(?=</caption>)", ">\n", answer)
-    answer = re.sub(">(?=<graphic)", ">\n", answer)
-    answer = re.sub(">(?=</fig>)", ">\n", answer)
-    # ...to improve identification & deletion of figure tags
-    answer = re.sub(r'\n?<fig[ \w="]+>(\n.+){4}\n</fig>', "", answer)
-    # Remove formula
-    answer = re.sub(r'\n\s*\\usepackage\{\w+\}', "", answer)
-    answer = re.sub(r'\n\s*\\setlength[-\w\{\}\\]+', "", answer)
-    answer = re.sub(r'\n\t+(\\)+begin', "\begin", answer)
-    answer = re.sub("<(?=tex-math)", "\n<", answer)
-    answer = re.sub("(?<=</tex-math)>", ">\n", answer)
-    answer = re.sub("<tex-math .+>\n+", "", answer)
-    answer = re.sub(r'<inline-graphic[ \w=":.]+/>', "", answer)
-    answer = re.sub(r'<(?=mml:math[ \w="]*>)', "\n<", answer)
-    answer = re.sub("((?<=</mml:math)>)\n?", ">\n", answer)
-    answer = re.sub(r'<mml:math[ \w="]*>.+</mml:math>\n', "", answer)
-    answer = re.sub("<alternatives>\n</alternatives>", "", answer)
+    # Handle references
+    answer = re.sub(r'</?ce:cross-refs?[- \w=".]*>', "", answer)
     answer = re.sub(
-        r'<inline-formula[ \w="]*>\n*</inline-formula>', "", answer)
-    answer = re.sub(r'<alternatives>\n*<graphic[ \w=".:]+/></alternatives>',
+        r'(&gt;)?</?ce:inter-ref[- \w=":/.%@#?&;,\[\]()~+*!\'$]*>(&lt;)?',
+        "", answer)
+    answer = re.sub(r'</?ce:intra-ref[- \w=":/.%@#?&;,\[\]()~+*!\'$]*>',
                     "", answer)
-    answer = re.sub(r'<disp-formula[ \w="]*>\n<label>.+</label>\n', "", answer)
-    answer = re.sub(r'</?disp-formula[ \w="]*>', "", answer)
+    answer = re.sub(r'</?xref[- \w=".]*/?>', " ", answer)
+    # Remove footnotes
+    answer = re.sub(r'<ce:footnote [ \w="]*>.+?</ce:footnote>', "", answer)
+    # Handle quotes
+    answer = re.sub(
+        r'<ce:displayed-quote[- \w=".]*> ?<ce:simple-para[- \w=".]*>',
+        "'", answer)
+    answer = re.sub(r'</ce:simple-para> ?<ce:simple-para[- \w=".]*>',
+                    "", answer)
+    answer = re.sub(r'</ce:simple-para> ?</ce:displayed-quote>', "'", answer)
+    answer = re.sub("</ce:simple-para> ?<ce:source>", "'", answer)
+    answer = re.sub("</ce:source> ?</ce:displayed-quote>", "", answer)
+    answer = re.sub("<disp-quote><p>", "<p>'", answer)
+    answer = re.sub("</p></disp-quote>", "'</p>", answer)
+    # Remove grant informations
+    answer = re.sub(r'</?ce:grant-sponsor[- \w=":/.%@#?&;,\[\]()~+*!\'$]*>',
+                    "", answer)
+    answer = re.sub(r'</?ce:grant-number[ \w="]*>', "", answer)
+    # Remove statements
+    answer = re.sub(r'<statement[- \w="]*>', "<p>", answer)
+    answer = re.sub(r'</statement>', "</p>", answer)
+    # Remove formula
+    answer = re.sub(r'<mml:math[ \w=".]*>.+?</mml:math>', "[Formula]", answer)
+    answer = re.sub("</?ce:display>", "", answer)
+    answer = re.sub(r'<disp-formula[ \w="]*>.+?</disp-formula>',
+                    "[Formula]", answer)
+    answer = re.sub(r'<ce:formula[ \w="]*>.+?</ce:formula>( *</ce:formula>)*',
+                    "[Formula]", answer)
+    answer = re.sub(r'<inline-formula[ \w="]*>.+?</inline-formula>',
+                    "[Formula]", answer)
+    answer = re.sub(r'<disp-formula[- \w="]*>.+?</disp-formula>',
+                    "[Formula]", answer)
+    # Remove figures
+    answer = re.sub(r'<ce:inline-figure[ \w=".]*>.+?</ce:inline-figure>',
+                    "[Inline Fig.]", answer)
+    answer = re.sub(r'<ce:figure[ \w="]*>.+?</ce:figure>', "[Fig.]", answer)
+    answer = re.sub(r'<fig[ \w="]*>.+?</fig>', "[Fig.]", answer)
+    # Avoid error after figures deletion by deleting floats section
+    answer = re.sub("<ce:floats>.+?</ce:floats>", "", answer)
+    # Remove graphics
+    answer = re.sub(r'<graphic[- \w=":/.]*>', "[Graphic]", answer)
+    answer = re.sub("<opening-graphic>.+?</opening-graphic>", "", answer)
+    answer = re.sub(r'<inline-graphic[- \w=":/.%@#?&;,\[\]()~+*!\'$]*>',
+                    "[Graphic]", answer)
     # Remove algorithms
-    answer = re.sub(r'<alg-item[ \w="]+>(\n.+){2}</alg-item>', "", answer)
-    answer = re.sub(r'<algorithm[- \w="]+>(\n.+){2}</algorithm>', "", answer)
+    answer = re.sub(r'<algorithm[- \w="]*>.+?</algorithm>', "[Algo]", answer)
     # Remove tables
-    answer = re.sub(r'<caption[ \w=":]+>.+\n</caption>', "", answer)
-    answer = re.sub(r'<(?=table-wrap[ \w="]+>)', "\n<", answer)
-    answer = re.sub("(?<=</table-wrap)>", ">\n", answer)
-    answer = re.sub("<table-wrap .+>(\n.*){2,5}</table-wrap>\n", "", answer)
+    answer = re.sub(r'<ce:table[ \w=":/.]*>.+?</ce:table>', "[Table]", answer)
+    answer = re.sub(r'<table-wrap[ \w="]*>.+?</table-wrap>', "[Table]", answer)
     # Remove Appendix
-    answer = re.sub(r'<app[\w\W]+</app>', "", answer)
+    answer = re.sub(r'<app[ \w="]*>.+?</app>', "", answer)
+    # Remove Aknowledgements
+    answer = re.sub("<ack>.+?</ack>", "", answer)
     # Fix multispaces
     answer = re.sub(" +", " ", answer)
 
     return answer
 
 
-def _process_paragraph(para: ET.Element) -> str:
+def _process_def_list(dlist: ET.Element, text: str) -> str:
+    """Extract text content from a def-list.
+
+    Args:
+        dlist: XML element containing a def-list
+        text: Text already found in this section.
+
+    Returns:
+        Text content of the def-list
+
+    Raises:
+        ValueError: Find an unknown subtag in the def-list.
+    """
+    for child in list(dlist):
+        # Elsevier term
+        if child.tag == f"{ns}def-term":
+            text += f"\n- {child.text}: "
+        # Elsevier definition
+        elif child.tag == f"{ns}def-description":
+            text += f"{child.text}"
+        # Springer
+        elif child.tag == "def-item":
+            text += f"- {child.find('./term')}: {child.find('./def/p')}"
+        else:
+            raise ValueError(f"Unknown def-list tag: {child.tag}")
+    return text
+
+
+def _process_paragraph(para: ET.Element, text: str) -> str:
     """Extract text content from a paragraph.
 
     Args:
         para: XML element containing a paragraph.
+        text: Text already found in this section.
 
     Returns:
         Text content of the paragraph.
@@ -101,7 +157,7 @@ def _process_paragraph(para: ET.Element) -> str:
     Raises:
         ValueError: Find an unknown subtag in the paragraph.
     """
-    text = f"{para.text}"
+    text += f"{para.text}"
 
     for child in list(para):
         # Ignore labels
@@ -113,6 +169,12 @@ def _process_paragraph(para: ET.Element) -> str:
         # Springer and IEEE
         elif child.tag == "list":
             items = child.findall(".//p")
+        elif child.tag == f"{ns}def-list" or child.tag == "def-list":
+            text = _process_def_list(child, text)
+        elif child.tag == f"{ns}section-title" or child.tag == "title":
+            text += f"{child.text}\n"
+        elif child.tag == f"{ns}para" or child.tag == "p":
+            text = _process_paragraph(child, text)
         else:
             raise ValueError(f"Unknown paragraph tag: {child.tag}")
         text += "\n".join([f"- {i.text}" for i in items])
@@ -143,7 +205,7 @@ def _process_section(section: ET.Element, text: str) -> str:
         elif child.tag == f"{ns}section-title" or child.tag == "title":
             text += f"{child.text}\n"
         elif child.tag == f"{ns}para" or child.tag == "p":
-            text += f"{_process_paragraph(child)}\n"
+            text = f"{_process_paragraph(child, text)}\n"
         elif child.tag == f"{ns}section" or child.tag == "sec":
             text = _process_section(child, text)
         else:
@@ -185,6 +247,9 @@ def process(answer: str, path: str, api: Api) -> None:
         if child.tag == f"{ns}section" or child.tag == "sec":
             with open(path, "a+") as f:
                 f.write(f"{_process_section(child, text)}\n")
+        elif child.tag == f"{ns}para" or child.tag == "p":
+            with open(path, "a+") as f:
+                f.write(f"{_process_paragraph(child, text)}\n")
         else:
             raise ValueError(f"Unknown root tag: {child.tag}")
 
@@ -231,7 +296,7 @@ def get_springer_abstract(answer: str) -> str:
             elif child.tag == "sec":
                 text += _process_section(child, text)
             elif child.tag == "p":
-                text = _process_paragraph(child)
+                text += _process_paragraph(child, text)
             else:
                 raise ValueError(f"Unknown root tag: {child.tag}")
 
